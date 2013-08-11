54f51e7e50fa7b4f4fada008fc473dfbe3c6f50b
Helal Ismail <helal@surveypocket.com>
2013-08-12 00:50:44 +0200

    Helal_Time2Fly

--- Time2Fly/src/com/ui/Home.java
+++ Time2Fly/src/com/ui/Home.java
@@ -1,15 +1,12 @@
 package com.ui;
 
 import android.app.Activity;
-import android.app.Dialog;
-import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.core.Constants;
 import com.core.Time2FlyApp;
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 
 public class Home extends Activity {
