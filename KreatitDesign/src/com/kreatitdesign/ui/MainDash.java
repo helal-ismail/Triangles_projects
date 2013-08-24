bbb35acb2d3efbaaaec7a28ae3865fa0948f0634
yehia-elsaka <yahya.elsaka@gmail.com>
2013-08-24 22:41:51 +0200

    24-8-2013, 10:40pm

--- KreatitDesign/src/com/kreatitdesign/ui/MainDash.java
+++ KreatitDesign/src/com/kreatitdesign/ui/MainDash.java
@@ -1,5 +1,9 @@
 package com.kreatitdesign.ui;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -7,18 +11,26 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.kreatitdesign.core.Constants;
+import com.kreatitdesign.network.RequestTask;
 
 @SuppressLint("InlinedApi")
 public class MainDash extends Activity {
 
 	Context myContext = this;
-	
+
 	String lock_key = "";
+	
+	ProgressBar prog_bar;
+	LinearLayout progress;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,41 +39,65 @@ public class MainDash extends Activity {
 
 		TextView pageTitle = (TextView) findViewById(R.id.page_title);
 		pageTitle.setText("Main Dashboard");
+		
+		
+		progress = (LinearLayout) findViewById(R.id.progress_bar_container);
+
+		prog_bar = (ProgressBar) findViewById(R.id.progress_bar);
+		prog_bar.getIndeterminateDrawable().setColorFilter(0xFFFF0000,
+				android.graphics.PorterDuff.Mode.MULTIPLY);
+		progress.setVisibility(View.GONE);
+		
 
 		// LOCK BUTTON
 		LinearLayout lock_btn = (LinearLayout) findViewById(R.id.lock_btn);
 		lock_btn.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 
-				AlertDialog.Builder alert = new AlertDialog.Builder( new ContextThemeWrapper(myContext, android.R.style.Theme_Holo_Dialog)); 
-				
+				AlertDialog.Builder alert = new AlertDialog.Builder(
+						new ContextThemeWrapper(myContext,
+								android.R.style.Theme_Holo_Dialog));
 
-		        alert.setTitle("System Lock");  
-		        alert.setMessage("Please enter your lock / unlock key ");
-		        alert.setIcon(R.drawable.icon_1);
+				alert.setTitle("System Lock");
+				alert.setMessage("Please enter your lock / unlock key ");
+				alert.setIcon(R.drawable.icon_1);
 
-		        // Set an EditText view to get user input   
-		        final EditText inputName = new EditText(new ContextThemeWrapper(myContext, android.R.style.Theme_Holo_Dialog));  
-		        alert.setView(inputName);  
+				// Set an EditText view to get user input
+				final EditText inputName = new EditText(
+						new ContextThemeWrapper(myContext,
+								android.R.style.Theme_Holo_Dialog));
+				alert.setView(inputName);
 
-		        alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {  
-		        public void onClick(DialogInterface dialog, int whichButton) {  
-		            lock_key = inputName.getText().toString();  
-		          }  
-		        
-		        }); 
-		        
-		        
-		        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-		            @Override
-		            public void onClick(DialogInterface dialog, int which) {
-		                dialog.cancel();
-		            }
-		        });
-		        
-		        
+				alert.setPositiveButton("OK",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int whichButton) {
+								lock_key = inputName.getText().toString();
 
-		        alert.show();
+								try {
+
+									LockTask task = new LockTask();
+									task.execute();
+
+								} catch (Exception e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								}
+
+							}
+
+						});
+
+				alert.setNegativeButton("Cancel",
+						new DialogInterface.OnClickListener() {
+							@Override
+							public void onClick(DialogInterface dialog,
+									int which) {
+								dialog.cancel();
+							}
+						});
+
+				alert.show();
 			}
 		});
 
@@ -70,7 +106,8 @@ public class MainDash extends Activity {
 		status_btn.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 
-				new AlertDialog.Builder(new ContextThemeWrapper(myContext, android.R.style.Theme_Holo_Dialog))
+				new AlertDialog.Builder(new ContextThemeWrapper(myContext,
+						android.R.style.Theme_Holo_Dialog))
 						.setTitle("System Status")
 						.setIcon(R.drawable.icon_1)
 						.setMessage("System Installed Successfully")
@@ -90,7 +127,8 @@ public class MainDash extends Activity {
 		panic_btn.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 
-				new AlertDialog.Builder(new ContextThemeWrapper(myContext, android.R.style.Theme_Holo_Dialog))
+				new AlertDialog.Builder(new ContextThemeWrapper(myContext,
+						android.R.style.Theme_Holo_Dialog))
 						.setTitle("Panic Alert")
 						.setIcon(R.drawable.icon_1)
 						.setMessage("Panic Alert from Tyrone ")
@@ -110,7 +148,8 @@ public class MainDash extends Activity {
 		ns_panic_btn.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 
-				new AlertDialog.Builder(new ContextThemeWrapper(myContext, android.R.style.Theme_Holo_Dialog))
+				new AlertDialog.Builder(new ContextThemeWrapper(myContext,
+						android.R.style.Theme_Holo_Dialog))
 						.setTitle("NS.Panic Alert")
 						.setIcon(R.drawable.icon_1)
 						.setMessage(
@@ -153,4 +192,66 @@ public class MainDash extends Activity {
 		});
 
 	}
+
+	// =================================================================================
+
+	private class LockTask extends RequestTask {
+		@Override
+		protected void onPreExecute() {
+			// TODO Auto-generated method stub
+			super.onPreExecute();
+
+			 progress.setVisibility(View.VISIBLE);
+
+			JSONArray arr = new JSONArray();
+			JSONObject arm = new JSONObject();
+			try 
+			{
+				arm.put("arm", lock_key);
+
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			arr.put(arm);
+
+			this.setupParams("Tyrone", 3, "demo0001", "tcousins", "getin123",
+					arr);
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			// TODO Auto-generated method stub
+			super.onPostExecute(result);
+			try {
+				
+				JSONObject object = new JSONObject(result);
+				JSONObject msg = object.optJSONObject("msg");
+				String disp = msg.optString("disp");
+
+				new AlertDialog.Builder(new ContextThemeWrapper(myContext,
+						android.R.style.Theme_Holo_Dialog))
+						.setTitle("System Lock")
+						.setIcon(R.drawable.icon_1)
+						.setMessage(disp)
+						.setPositiveButton("OK",
+								new DialogInterface.OnClickListener() {
+									public void onClick(DialogInterface dialog,
+											int which) {
+										dialog.cancel();
+									}
+								}).show();
+				
+
+				 progress.setVisibility(View.GONE);
+
+				Log.d(Constants.TAG, "RESULT : " + result);
+
+			} catch (Exception e) {
+				// TODO: handle exception
+				Log.d(Constants.TAG, "Exception : " + e.getMessage());
+			}
+		}
+	}
+
 }
