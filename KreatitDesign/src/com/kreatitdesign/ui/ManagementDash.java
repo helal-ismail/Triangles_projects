e3e007a51302bd159b8a0dc43a1583e94022909e
Helal Ismail <helal@surveypocket.com>
2013-09-06 23:37:59 +0200

    6-9-2013, 11:30pm

--- KreatitDesign/src/com/kreatitdesign/ui/ManagementDash.java
+++ KreatitDesign/src/com/kreatitdesign/ui/ManagementDash.java
@@ -1,10 +1,39 @@
 package com.kreatitdesign.ui;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextThemeWrapper;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.kreatitdesign.core.Constants;
+import com.kreatitdesign.core.GlobalState;
+import com.kreatitdesign.core.KreatitDesignApp;
+import com.kreatitdesign.core.User;
+import com.kreatitdesign.network.RequestTask;
+
 public class ManagementDash extends Activity {
+	
+	Context myContext = this;
+	
+	ProgressBar prog_bar;
+	LinearLayout progress;
+
+	KreatitDesignApp bindAlertApp = new KreatitDesignApp();
+	
+	GlobalState globalState = GlobalState.getInstance();
+	User user;
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +43,46 @@ public class ManagementDash extends Activity {
 		TextView pageTitle = (TextView) findViewById(R.id.page_title);
 		pageTitle.setText("Management Dashboard");
 		
+		
+		user = globalState.user;
+
+		progress = (LinearLayout) findViewById(R.id.progress_bar_container);
+
+		prog_bar = (ProgressBar) findViewById(R.id.progress_bar);
+		prog_bar.getIndeterminateDrawable().setColorFilter(0xFFFF0000,
+				android.graphics.PorterDuff.Mode.MULTIPLY);
+		progress.setVisibility(View.GONE);
+
+		
+		
+		// OP_MODE BUTTON
+				LinearLayout status_btn = (LinearLayout) findViewById(R.id.op_mode_btn);
+				status_btn.setOnClickListener(new View.OnClickListener() {
+					public void onClick(View v) {
+
+						Intent i = new Intent(myContext, Op_Mode.class);
+						startActivityForResult(i, 700);
+						overridePendingTransition(R.anim.slide_in_right,
+								R.anim.slide_out_left);
+
+					}
+				});
+		
+		
 	}
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
 }
---
+++ KreatitDesign/src/com/kreatitdesign/ui/Op_Mode.java
@@ -0,0 +1,210 @@
+package com.kreatitdesign.ui;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextThemeWrapper;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.kreatitdesign.core.Constants;
+import com.kreatitdesign.core.GlobalState;
+import com.kreatitdesign.core.KreatitDesignApp;
+import com.kreatitdesign.core.User;
+import com.kreatitdesign.network.RequestTask;
+
+@SuppressLint("InlinedApi")
+public class Op_Mode extends Activity {
+
+	Context myContext = this;
+
+	ProgressBar prog_bar;
+	LinearLayout progress;
+
+	KreatitDesignApp bindAlertApp = new KreatitDesignApp();
+
+	GlobalState globalState = GlobalState.getInstance();
+	User user;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_op_mode);
+
+		TextView pageTitle = (TextView) findViewById(R.id.page_title);
+		pageTitle.setText("Operational Mode");
+
+		user = globalState.user;
+
+		progress = (LinearLayout) findViewById(R.id.progress_bar_container);
+
+		prog_bar = (ProgressBar) findViewById(R.id.progress_bar);
+		prog_bar.getIndeterminateDrawable().setColorFilter(0xFFFF0000,
+				android.graphics.PorterDuff.Mode.MULTIPLY);
+		progress.setVisibility(View.GONE);
+
+		// Get Mode BUTTON
+		Button get_mode_btn = (Button) findViewById(R.id.get_mode);
+		get_mode_btn.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+
+				try {
+
+					OpModeTask task = new OpModeTask();
+					task.taskNumber = 4;
+					task.title = "Operational Mode";
+
+					JSONArray arr = new JSONArray();
+					arr.put("op_mode");
+					task.arr = arr;
+
+					task.execute();
+
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+		});
+
+		// Get Mode BUTTON
+		Button update_mode_btn = (Button) findViewById(R.id.update_mode);
+		update_mode_btn.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				AlertDialog.Builder alert = new AlertDialog.Builder(
+						new ContextThemeWrapper(myContext,
+								android.R.style.Theme_Holo_Dialog));
+
+				alert.setTitle("Operational Mode");
+				alert.setMessage("Please enter the new mode ");
+				alert.setIcon(R.drawable.icon_1);
+
+				// Set an EditText view to get user input
+				final EditText inputName = new EditText(
+						new ContextThemeWrapper(myContext,
+								android.R.style.Theme_Holo_Dialog));
+				alert.setView(inputName);
+
+				alert.setPositiveButton("Update",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int whichButton) {
+								String mode = inputName.getText().toString();
+
+								try {
+
+									OpModeTask task = new OpModeTask();
+									task.taskNumber = 4;
+									task.title = "Update Op_Mode";
+
+									JSONArray arr = new JSONArray();
+									JSONObject arm = new JSONObject();
+									try {
+										arm.put("op_mode", mode);
+
+									} catch (JSONException e) {
+										// TODO Auto-generated catch block
+										e.printStackTrace();
+									}
+									arr.put(arm);
+
+									task.arr = arr;
+
+									task.execute();
+
+								} catch (Exception e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								}
+
+							}
+
+						});
+
+				alert.setNegativeButton("Cancel",
+						new DialogInterface.OnClickListener() {
+							@Override
+							public void onClick(DialogInterface dialog,
+									int which) {
+								dialog.cancel();
+							}
+						});
+
+				alert.show();
+			}
+		});
+	}
+
+
+	// =================================================================================
+
+	private class OpModeTask extends RequestTask {
+
+		int taskNumber;
+		String title;
+		JSONArray arr;
+
+		@Override
+		protected void onPreExecute() {
+			// TODO Auto-generated method stub
+			super.onPreExecute();
+
+			progress.setVisibility(View.VISIBLE);
+
+			this.setupParams(user.name, taskNumber, user.devID, user.userName,
+					user.password, arr);
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			// TODO Auto-generated method stub
+			super.onPostExecute(result);
+			try {
+
+				JSONObject object = new JSONObject(result);
+				JSONObject msg = object.optJSONObject("msg");
+
+				String disp = "";
+				disp = msg.optString("op_mode");
+
+				if (disp.equals(""))
+					disp = msg.optString("disp");
+
+				new AlertDialog.Builder(new ContextThemeWrapper(myContext,
+						android.R.style.Theme_Holo_Dialog))
+						.setTitle(title)
+						.setIcon(R.drawable.icon_1)
+						.setMessage(disp)
+						.setPositiveButton("OK",
+								new DialogInterface.OnClickListener() {
+									public void onClick(DialogInterface dialog,
+											int which) {
+										dialog.cancel();
+									}
+								}).show();
+
+				progress.setVisibility(View.GONE);
+
+				Log.d(Constants.TAG, "RESULT : " + result);
+
+			} catch (Exception e) {
+				// TODO: handle exception
+				Log.d(Constants.TAG, "Exception : " + e.getMessage());
+			}
+		}
+	}
+
+}
